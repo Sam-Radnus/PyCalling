af5bacd2e735164e03e55d084be75ccd616ce456
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-06-15 19:38:49 +0530

    Implemented Login

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../Styles/lobby.css'
 import {Link ,Navigate,useNavigate,useLocation} from 'react-router-dom';
+import { useDispatch } from "react-redux";
 import { useState } from 'react';
+import { login } from "./../features/userSlice";
 import Room from './Room';
 function Lobby() {
     let navigate=useNavigate();
     let [name,setName]=useState('');
     let [room,setRoom]=useState('');
+    const dispatch=useDispatch();
   return (
     <div>
          
@@ -40,8 +43,15 @@ function Lobby() {
                         console.log("name:",name)
                         console.log("room:",room)       
                         navigate(`/Room/${room}`)       
+                        dispatch(
+                            login({
+                                name:name,
+                                room:room,
+                                loggedIn:true,
+                            })
+                        )
                         sessionStorage.setItem('name',name)     
-                        return <Room name={name}/>
+                        return <Room />
                    }} type="submit">Go to Room 
                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
                     </button>
