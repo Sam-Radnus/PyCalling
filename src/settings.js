85d07f3c291953049cb1c4bc616dd8e804b8abb5
samsundar <samradnus2001@gmail.com>
2022-06-25 11:32:12 +0530

    Bug Fixing

--- src/settings.js
+++ src/settings.js
@@ -13,5 +13,6 @@ import {
 const appid="9e4b87cc837448969b97b4301e2aca92";
 export const config ={ mode:"rtc",codec:"vp8",appid:appid}
 export const useClient=createClient(config);
+
 export const useChannel=createChannel('test');
 export const useMicrophoneAndCameraTracks=createMicrophoneAndCameraTracks();
